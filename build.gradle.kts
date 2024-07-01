84fe49a8560da130cff28dfee88ce76f3ce320c4
RedMagic <lijnbelt13@gmail.com>
2024-07-01 23:37:52 +0200

    Support 1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 group = "com.redmagic"
 version = "1.0"
 
-val mcVersion = "1.20.4"
+val mcVersion = "1.21"
 
 repositories {
     mavenCentral()
@@ -28,7 +28,7 @@ repositories {
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("com.undefined:api:0.5.44:mapped")
+    implementation("com.undefined:api:0.5.47:mapped")
 
     implementation("com.google.code.gson:gson:2.11.0")
 }
